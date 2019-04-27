0892cd6e466fe14b25aec89294e50e6c20b0ca90
aritra bhawani <23428977+aritra-bhawani@users.noreply.github.com>
2019-04-27 05:45:02 +0530

    credentials delivery from new node to base nodes

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from termcolor import colored
+print colored('Initializing Dependencies...','yellow')
 import socket
 from thread import *
 from Crypto.Cipher import AES
@@ -10,36 +12,9 @@ import os
 import sys
 import sqlite3
 
-def get_ip():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	try:
-		# doesn't even have to be reachable
-		s.connect(('10.255.255.255', 1))
-		IP = s.getsockname()[0]
-	except:
-		IP = '127.0.0.1'
-	finally:
-		s.close()
-	return IP
-
-host=''
-port=5000
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connections = {}
-try:
-	s.bind((host,port))
-	print ("SERVER STARTED")
-	print ("IP => "+ get_ip())
-except socket.error as e:
-	print(str(e))
-
-s.listen(10)
-print ("port => "+str(s.getsockname()[1]))
-print ("listening...\n")
-
 if __name__=="__main__":
-	#Intializing 4 Users 
-	u=[[23,1234],[24,12345],[25,4321],[26,54321]]
+	#Intializing 7 Users 
+	u=[[23,1234],[24,12345],[25,4321],[26,54321],[27,1234],[28,4321],[29,12345]]
 	con=sqlite3.connect('data_base.db')
 	c = con.cursor()
 	c.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -66,11 +41,10 @@ if __name__=="__main__":
 	con.commit()
 	con.close()	
 
-
-def d_verify(id,key):
+def d_verify(d_id,key):
 	con=sqlite3.connect('data_base.db')
 	c = con.cursor()
-	c.execute("SELECT * FROM DEVICES where D_ID = ?", (id,))
+	c.execute("SELECT * FROM DEVICES where D_ID = ?", (d_id,))
 	result = c.fetchall()
 	con.commit()
 	con.close()
@@ -85,11 +59,11 @@ def d_verify(id,key):
 		else:
 			return([True,"Device Verified Successfully!\nSharing Key..."])
 
-def d_stat_up(id,val):
+def d_stat_up(d_id,val):
 	con=sqlite3.connect('data_base.db')
 	c = con.cursor()
-	c.execute("UPDATE DEVICES SET D_STAT = ? WHERE D_ID = ?", (val,id))
-	c.execute("SELECT * FROM DEVICES WHERE D_ID=?",(id,))
+	c.execute("UPDATE DEVICES SET D_STAT = ? WHERE D_ID = ?", (val,d_id))
+	c.execute("SELECT * FROM DEVICES WHERE D_ID=?",(d_id,))
 	print "Updated Device Status in DB : ",c.fetchall()
 	con.commit()
 	con.close()
@@ -102,7 +76,7 @@ def d_add(d_id,start_time,stop_time,unit_slab,has):
 	con.commit()
 	con.close()
 
-def verify(conn):
+def verify(conn,n_id):
 	ar=(conn.recv(1024).rstrip("\n\r")).split(',') #2-2
 	print ("Device ID => "+str(ar[0])+"\nDevice Key => "+str(ar[-1]))
 	result=d_verify(int(ar[0]),int(ar[-1]))
@@ -111,23 +85,22 @@ def verify(conn):
 		return ([True,int(ar[0])])
 	else:
 		conn.send(result[1])#3-1
-		connections.pop(id)
+		connections.pop(n_id)
 		return ([False,int(ar[0])])	
 
-def DHK_exc_s(key,conn,d_id):
+def DHK_exc_s(key,conn,d_id,n_id):
 	sharedPrime=9999999900000001 # a random prime to be chosen
-	sharedBase=102124190 # a random number
+	sharedBase=random.randint(100000000,999999999) #102124190 # a random number
 	conn.send(str(sharedPrime)+","+str(sharedBase)) #5-1
 	try:
 		A=long(conn.recv(1024).rstrip("\n\r")) #6-2
 		B=(sharedBase ** key) % sharedPrime 
 	except ValueError:
-		connections.pop(id)
+		connections.pop(n_id)
 		print ("Error Occured!")
 		print "Connections List : ",connections
 		conn.close()
 		print ("Connection with "+str(d_id)+" is Closed")
-		# print ('Connection Closed')
 	conn.send(str(B)) #7-1
 	shared_key = (A**key) % sharedPrime
 	return shared_key
@@ -188,37 +161,57 @@ def dig_sig_gen(q,d_e,n,string,sg=0):
 		if h_val==h_cal : return True
 		else : return False
 
-def close_conn(d_id,id):
+def close_conn(d_id,n_id):
 	print ("Token Verification : Failed\nConnection with "+str(d_id)+" is Closed")
 	d_stat_up(d_id,0)
-	connections.pop(id)
+	connections.pop(n_id)
 	print "Connections List : ",connections
 	conn.close()
 
-def client_thread(conn,id):
+#==========================
+def base_node(n_id,conn,d_id,shared_key,n_s,d,n_c,e_c):
+	print('base node identified')
+	b_n_ar.append(n_id)
+	print "Base Nodes : ",b_n_ar
+
+#==========================
+def node(n_id,conn,d_id,shared_key,n_s,d,n_c,e_c):
+	print('node identified')
+	old_hash_value,t_id='abc',0 # To Be Used During Reading Verification
+	data=enc_dec('d',shared_key,conn.recv(1024).rstrip("\n\r")).split(',') #14-2
+	n_ip,n_port=data[0],data[1]
+	print('Received IP and port address : '+n_ip+' & '+n_port+'\nSending list of base nodes')
+	conn.send(enc_dec('e',shared_key,','.join(str(x) for x in b_n_ar)))# 15-1
+	data=[i.split(':') for i in enc_dec('d',shared_key,conn.recv(1024).rstrip("\n\r")).split('|')] #16-2
+	print ('Nominated based nodes along with sharedBase for '+str(n_id)+'|'+str(d_id)+' => '+str(data))
+	for i in data:
+		connections[int(i[0])].send(enc_dec('e',connections_credentials[int(i[0])]['shared_key'],str(n_id)+','+str(n_ip)+','+str(n_port)+','+str(i[1])+','+str(i[0])))# 15-1
+	print ("Credentials have been sent to the respected Base Nodes")
+
+#==========================
+def client_thread(n_id,conn):
 	d_id,key = '',''
-	f=0# flags
+	f=0 # flags
 	shared_key,serverSecret = '',random.randint(5,20) # used in Diffie-hellman
 	n_s,e_s,d = 0,0,0
 	n_c,e_c = 0,0 # RSA Signature Parameters of Client
-	old_hash_value,t_id='abc',0 # To Be Used During Reading Verification
-	conn.send('C') #1-1
+	conn.send('C') # 1-1
 	while True:
 		if f==0:
 			# Device Verification
-			v=verify(conn)
+			v=verify(conn,n_id)
 			time.sleep(.1)
 			if v[0]:
-				conn.send('S') #4-1
+				conn.send('S') # 4-1 - success
 				d_id=v[1]
 			else:
-				conn.send('F') #4-1
+				conn.send('F') # 4-1 - failed
 				break
 
 			# Key exchange using Diffie-Helman
 			kc=0
 			while kc==0:
-				shared_key=str(DHK_exc_s(serverSecret,conn,d_id))
+				shared_key=str(DHK_exc_s(serverSecret,conn,d_id,n_id))
 				# kl=len(shared_key)
 				if len(shared_key)==16:
 					#checking key symmetricity
@@ -232,7 +225,7 @@ def client_thread(conn,id):
 					else:
 						print("Key Validation Failed")
 			print ("Shared Key => "+str(shared_key))
-			print ("***** Hereafter, All the Data Tranfer Will Be Encrypted. But for Our Convinience, Decrypted Values Will be Printed *****")
+			print colored("***** Hereafter, All the Data Tranfer Will Be Encrypted. But for Our Convinience, Decrypted Values Will be Printed *****",'red')
 
 			#Computing parameters for generating digital signature using RSA
 			print ("Generating Parameters for Digital Signature...(Wait)")
@@ -255,62 +248,54 @@ def client_thread(conn,id):
 			# print(dig_sig_gen('v',e_c,n_c,ar[0],sg=int(ar[1])))
 			if dig_sig_gen('v',e_c,n_c,ar[0],int(ar[1])):
 				if d_stat_up(d_id,1):
+					connections_credentials.update({n_id:{'d_id':d_id,'shared_key':shared_key,'n_s':n_s,'d':d,'n_c':n_c,'e_c':e_c}})
+					print (connections_credentials)
 					f=1
-					print("\n")
-		else:
-			#Validating Before Data Transmission
-			data=enc_dec('d',shared_key,conn.recv(1024).rstrip("\n\r")) #a-2
-			print("Token ID Received from Client => "+data)
-			try:
-				n_t_id=int(data)
-			except:
-				conn.send(enc_dec('e',shared_key,"Error Occured!\nToken Verification : Failed")) #b-1
-				close_conn(d_id,id)
-				break
-			if t_id>=n_t_id:
-				conn.send(enc_dec('e',shared_key,"Error Occured!\nToken Verification : Failed")) #b-1
-				close_conn(d_id,id)
-				break
-			print ("Token Verification : Successfully \nSending Old Hash => "+old_hash_value)
-			conn.send(enc_dec('e',shared_key,old_hash_value)) #b-1
-			
-			data=conn.recv(1024).rstrip("\n\r") #value receiving #c-1
-			if not data:
-				print ("Connection with "+str(d_id)+" is Closed")
-				d_stat_up(d_id,0)
-				connections.pop(id)
-				print "Connections List : ",connections
-				conn.close()
-				break
-			print ("Data Received: "+str(data))
-			data=enc_dec("d",shared_key,data)
-			print ("Data Retrieved After Decrypting: "+data)
-			data=data.split('|')
-			if (dig_sig_gen('v',e_c,n_c,data[0],int(data[1]))) and (n_t_id==int(data[0].split(',')[0])): # signature and token validation
-				print ("Token and Signature Verification : Successfully")
-				# ****** Put data into DB
-				t=data[0].split(',')
-			else:
-				close_conn(d_id,id)
-
-			#generating (16 bit) New Hash Value = Hash(Present readings + Old Hash Value)
-			new_hash_value = int(hashlib.sha256((data[0]+old_hash_value).encode('utf-8')).hexdigest(), 16) % 10**16
-			print ("Sending New Hash => "+str(new_hash_value)+"\n")
-			conn.send(enc_dec('e',shared_key,str(new_hash_value)+"|"+str(dig_sig_gen('g',d,n_s,str(new_hash_value))))) #d-1
-			d_add(d_id,t[1],t[2],t[3],new_hash_value)
-			print ('Logging the Tuple into The DB')
-			old_hash_value,t_id=str(new_hash_value),n_t_id
+					break
+	if f!=0:
+		#sepecifying node 23, 24, 25 as the base nodes. But it is to be fetched from the DB in the next update.
+		node(n_id,conn,d_id,shared_key,n_s,d,n_c,e_c) if d_id not in [23,24,25,26] else base_node(n_id,conn,d_id,shared_key,n_s,d,n_c,e_c) 
 	return
 
+#============
+def get_ip():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	try:
+		# doesn't even have to be reachable
+		s.connect(('10.255.255.255', 1))
+		IP = s.getsockname()[0]
+	except:
+		IP = '127.0.0.1'
+	finally:
+		s.close()
+	return IP
 
+host=''
+port=5000
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connections,connections_credentials = {},{}
+try:
+	s.bind((host,port))
+	print colored("SERVER STARTED\nIP => "+ get_ip(),'green')
+except socket.error as e:
+	print colored(str(e),'red')
+
+s.listen(10)
+print colored("port => "+str(s.getsockname()[1])+"\nListening...\n",'green')
+#=======================
+
+n_id,b_n_ar=1,[]
 while __name__=="__main__":
 	try:
 		conn , addr = s.accept()
 		print('connected to:' +addr[0] +":"+str(addr[1]))
-		id = random.randint(1,9999999999)
-		start_new_thread(client_thread,(conn,id))
-		connections.update({id:conn})
+		start_new_thread(client_thread,(n_id,conn))
+		connections.update({n_id:conn})
+		n_id+=1
 		print "Connections List : ",connections
 	except KeyboardInterrupt:
-		print ("\nServer is Stopped!")
-		sys.exit()	
+		print colored("\nServer is Stopped!",'red')
+		print('Resetting device status in the DB')
+		d_id_list=[23,24,25,26,27,28,29]
+		[d_stat_up(i,0) for i in d_id_list]
+		sys.exit()
